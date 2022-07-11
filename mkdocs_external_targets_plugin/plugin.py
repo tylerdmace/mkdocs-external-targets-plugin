93f66ddf1cbd0928516cd001eeef29dd10b4505d
Tyler Mace <tylerdmace@gmail.com>
2022-07-11 15:07:53 -0600

    on_page_content

--- mkdocs_external_targets_plugin/plugin.py
+++ mkdocs_external_targets_plugin/plugin.py
@@ -1,7 +1,16 @@
+from bs4 import BeautifulSoup
+
 from mkdocs import utils as mkdocs_utils
 from mkdocs.config import config_options, Config
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.pages import Page
 
 class ExternalTargets(BasePlugin):
-  pass
+  def on_page_content(self, content, **kwargs):
+    html = BeautifulSoup(content)
+    anchors = html.find_all('a')
+    
+    for anchor in anchors:
+      anchor["target"] = "_blank"
+
+    return html
