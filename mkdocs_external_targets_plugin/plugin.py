c7abc1cd94711bceb1c3bf5550dd6ee28e43ea8e
Tyler Mace <tylerdmace@gmail.com>
2022-07-11 16:23:26 -0600

    Clean template DOM and not just page DOM

--- mkdocs_external_targets_plugin/plugin.py
+++ mkdocs_external_targets_plugin/plugin.py
@@ -1,3 +1,4 @@
+from bleach import clean
 from bs4 import BeautifulSoup
 
 from mkdocs import utils as mkdocs_utils
@@ -5,17 +6,19 @@ from mkdocs.config import config_options, Config
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.pages import Page
 
+def cleanExternalLinks(content):
+  html = BeautifulSoup(content, features = "html5lib")
+  anchors = html.find_all('a')
+  
+  for anchor in anchors:
+    if anchor["href"].find('http') != -1:
+      anchor["target"] = "_blank"
+
+  return html
+
 class ExternalTargets(BasePlugin):
   def on_post_template(self, content, **kwargs):
-    print(content)
-    return content
+    return cleanExternalLinks(content)
 
   def on_page_content(self, content, **kwargs):
-    html = BeautifulSoup(content, features = "html5lib")
-    anchors = html.find_all('a')
-    
-    for anchor in anchors:
-      if anchor["href"].find('http') != -1:
-        anchor["target"] = "_blank"
-
-    return html
+    return cleanExternalLinks(content)
