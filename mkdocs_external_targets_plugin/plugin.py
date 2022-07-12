78f743b89d5c931011580eb10a021e7ad21569c9
Tyler Mace <tylerdmace@gmail.com>
2022-07-11 18:10:09 -0600

    Final fixes

--- mkdocs_external_targets_plugin/plugin.py
+++ mkdocs_external_targets_plugin/plugin.py
@@ -1,4 +1,4 @@
-from bleach import clean
+import xml
 from bs4 import BeautifulSoup
 
 from mkdocs import utils as mkdocs_utils
@@ -9,17 +9,13 @@ from mkdocs.structure.pages import Page
 def cleanExternalLinks(content):
   html = BeautifulSoup(content, features = "html5lib")
   anchors = html.find_all('a')
-  
+
   for anchor in anchors:
     if anchor["href"].find('http') != -1:
       anchor["target"] = "_blank"
 
-  return html
+  return html.prettify()
 
 class ExternalTargets(BasePlugin):
-  def on_post_template(self, content, **kwargs):
-    print(content)
-    return cleanExternalLinks(content)
-
-  def on_page_content(self, content, **kwargs):
+  def on_post_page(self, content, **kwargs):
     return cleanExternalLinks(content)
